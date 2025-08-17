c1065ba63255128962a6989b7ab58484c9bd9ed6
istanculea <stanculea.ionut.93@gmail.com>
2025-08-17 16:50:58 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import BlogIndex from "./pages/BlogIndex";
 import BlogPost1 from "./pages/BlogPost1";
 import BlogPost2 from "./pages/BlogPost2";
 import BlogPost3 from "./pages/BlogPost3";
+import BlogPost4 from "./pages/BlogPost4";
+import PostLinuxACL from "./pages/PostLinuxACL";
 
 const queryClient = new QueryClient();
 
@@ -21,9 +24,12 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
-            <Route path="/blog/azure-terraform" element={<BlogPost1 />} />
-            <Route path="/blog/grafana-monitoring" element={<BlogPost2 />} />
-            <Route path="/blog/kubernetes-best-practices" element={<BlogPost3 />} />
+            <Route path="/blog" element={<BlogIndex />} />
+            <Route path="/blog/post-deployment-monitoring" element={<BlogPost1 />} />
+            <Route path="/blog/mongodb-setup" element={<BlogPost2 />} />
+            <Route path="/blog/serverless-website-recaptcha" element={<BlogPost3 />} />
+            <Route path="/blog/openvpn-server-setup" element={<BlogPost4 />} />
+            <Route path="/blog/linux-acl-management" element={<PostLinuxACL />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
